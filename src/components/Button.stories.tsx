f43548ccbc9691474badc9894468668ec1aaf83e
koyaaoyama <aoyama.ko0920@gmail.com>
2024-09-02 18:20:14 +0900

    first commit

---
+++ src/components/Button.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Button from "./Button";
+
+const meta = {
+  title: "Button",
+  component: Button,
+} satisfies Meta<typeof Button>;
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const HelloButton: Story = {
+  args: {
+    children: "Hello World",
+  },
+};
