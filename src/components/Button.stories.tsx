ece61aaad0b814b3b32c2be72f18ef752251a313
koyaaoyama <aoyama.ko0920@gmail.com>
2024-09-04 17:33:19 +0900

    Actions

--- src/components/Button.stories.tsx
+++ src/components/Button.stories.tsx
@@ -1,9 +1,23 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import Button from "./Button";
+// import { action } from "@storybook/addon-actions";
 
 const meta = {
   title: "Common/Button",
   component: Button,
+  argTypes: {
+    color: {
+      options: ["primary", "default", "danger"],
+      control: { type: "radio" },
+    },
+    size: {
+      options: ["sm", "base", "lg"],
+      control: { type: "select" },
+    },
+    handleClick: {
+      action: "clicked",
+    },
+  },
 } satisfies Meta<typeof Button>;
 
 export default meta;
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,12 +2,22 @@ import "./button.css";
 
 type Props = {
   children: React.ReactNode;
-  color?: string;
-  size?: string;
+  color?: "default" | "primary" | "danger";
+  size?: "base" | "sm" | "lg";
+  handleClick?: () => void;
 };
 
-function Button({ children, color = "default", size = "base" }: Props) {
-  return <button className={`${color} ${size}`}>{children}</button>;
+function Button({
+  children,
+  color = "default",
+  size = "base",
+  handleClick = () => console.log("click"),
+}: Props) {
+  return (
+    <button className={`${color} ${size}`} onClick={handleClick}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
