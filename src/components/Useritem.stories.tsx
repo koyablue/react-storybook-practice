b14c0de4da8a4d00915b8d69f3aecf9bf88ba2dd
koyaaoyama <aoyama.ko0920@gmail.com>
2024-09-04 17:47:36 +0900

    Loader

---
+++ src/components/Useritem.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import UserItem from "./UserItem";
+
+const meta = {
+  title: "Common/UserItem",
+  component: UserItem,
+  tags: ["autodocs"],
+} satisfies Meta<typeof UserItem>;
+
+export default meta;
+
+type Story = StoryObj<typeof UserItem>;
+
+export const Default: Story = {
+  args: {
+    id: "1",
+    name: "John Doe",
+  },
+};
+
+export const FetchData: Story = {
+  loaders: [
+    async () => ({
+      user: await (
+        await fetch("https://jsonplaceholder.typicode.com/users/1")
+      ).json(),
+    }),
+  ],
+  render: (args, { loaded: { user } }) => (
+    <UserItem {...args} id={user.id} name={user.name} />
+  ),
+};
